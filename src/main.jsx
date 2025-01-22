63f58367b843b9680959c88d8e857c8ba24b7baa
ChanVimean <mrrmeanvlogger@gmail.com>
2025-01-22 23:28:33 +0700

    Conenct Contact Route

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import { ProductProvider } from './store/ProductProvider.jsx'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import CartPage from './pages/CartPage.jsx'
+import Contact from './pages/Contact.jsx'
 
 AOS.init()
 
@@ -25,7 +26,7 @@ createRoot(document.getElementById('root')).render(
             <Route path={'/'} element={<Home />} />
             <Route path={'/product'} element={<Product />} />
             <Route path={'/cart'} element={<CartPage />} />
-            {/* <Route path={'/contact'} element={<Product />} /> */}
+            <Route path={'/contact'} element={<Contact />} />
           </Route>
           <Route path={'/*'} element={<PageNotFound/>} />
         </Routes>
